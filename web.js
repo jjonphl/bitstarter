6fd05a88750fb92aa853b5e6583af558931fc2b2
Miguel Manese <jjonphl@falt>
2013-07-08 00:26:49 +0800

    - assignment 3 part 1

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var index = fs.readFileSync('index.html'); // this is a Buffer  
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
